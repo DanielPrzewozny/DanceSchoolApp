a7716e425abe68f98d2c7d32461bd0a37b135531
DanielPrzewozny <daniel.przewozny@yahoo.com>
2023-03-13 13:20:50 +0100

    Apprentice model added

---
+++ src/DanceSchoolAPI.Common/Models/Apprentice/Apprentice.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using DanceSchoolAPI.Common.Enums;
+using DanceSchoolAPI.Common.Models.Students;
+
+namespace DanceSchoolAPI.Common.Models.Apprentice;
+
+[Table("dbo.Apprentices")]
+public class Apprentice : User
+{
+    public override UserRole Role => UserRole.Apprentice;
+    public DanceGroup DanceGroups { get; set; }
+}
